4240942126cc8cb93fe61dfad822153dd1eec97d
wodrow <wodrow451611cv@gmail.com>
2016-11-28 16:52:54 +0800

    update

--- book/modules/test/views/default/index.php
+++ book/modules/test/views/default/index.php
@@ -1,3 +1,7 @@
+<?php
+\common\assets\VideoJs::register($this);
+?>
+
 <div class="test-default-index">
     <h1><?= $this->context->action->uniqueId ?></h1>
     <p>
@@ -12,5 +16,12 @@
     <p>
         <code><?=$this->params['abc'] ?></code>
         <code><?=Yii::$app->cache->get('x') ?></code>
+    </p>
+    <p>
+        <video id="really-cool-video" class="video-js vjs-default-skin" controls
+               preload="auto" width="640" height="264" poster="http://7xsbq6.com1.z0.glb.clouddn.com/datas11050606281732.png"
+               data-setup='{}'>
+            <source src="http://7xsbq6.com1.z0.glb.clouddn.com/test1" type="video/mp4">
+    </video>
     </p>
 </div>
