338faa8c3f15189ce30d4def147a9f5bbd60ba02
wodrow <wodrow451611cv@gmail.com>
2016-11-30 10:52:14 +0800

    add oauth-client

--- book/modules/test/controllers/DefaultController.php
+++ book/modules/test/controllers/DefaultController.php
@@ -5,15 +5,32 @@ namespace book\modules\test\controllers;
 use common\components\config\Config;
 use yii\web\Controller;
 
-/**
- * Default controller for the `test` module
- */
 class DefaultController extends Controller
 {
+    public function actions()
+    {
+        return [
+            'auth' => [
+                'class' => \yii\authclient\AuthAction::className(),
+                'successCallback' => [$this, 'successCallback'],
+            ],
+        ];
+    }
+
     /**
-     * Renders the index view for the module
-     * @return string
+     * Success Callback
+     * @param QqAuth|WeiboAuth $client
+     * @see http://wiki.connect.qq.com/get_user_info
+     * @see http://stuff.cebe.cc/yii2docs/yii-authclient-authaction.html
      */
+    public function successCallback($client) {
+        $id = $client->getId(); // qq | sina | weixin
+        $attributes = $client->getUserAttributes(); // basic info
+        $openid = $client->getOpenid(); //user openid
+        $userInfo = $client->getUserInfo(); // user extend info
+        var_dump($id, $attributes, $openid, $userInfo);
+    }
+
     public function actionIndex()
     {
         \Yii::$app->cache->set('x', '654');
--- book/modules/test/views/default/test1.php
+++ book/modules/test/views/default/test1.php
@@ -5,11 +5,23 @@
  * Date: 11/29/16
  * Time: 5:23 PM
  */
+use yii\authclient\widgets\AuthChoice;
+
 ?>
 
 <div class="test-default-test1">
     <h1><?= $this->context->action->uniqueId ?></h1>
     <p>
-        123465
+        <?php $authAuthChoice = AuthChoice::begin([
+            'baseAuthUrl' => ['/test/default/auth'],
+        ]) ?>
+        <ul>
+            <?php foreach ($authAuthChoice->getClients() as $client): ?>
+                <li>
+                    <?= $authAuthChoice->clientLink($client) ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php AuthChoice::end(); ?>
     </p>
 </div>
